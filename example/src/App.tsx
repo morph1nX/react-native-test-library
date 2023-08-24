34f57594efe1f3932d5066914f48e006b132a91e
Rhythm Aich <rhythmaich@gmail.com>
2023-08-24 13:17:10 +0530

    chore: modified logger

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { add, loggerLibrary } from 'react-native-test-library';
+import LoggerObject from 'react-native-test-library';
 
 export default function App() {
   const [result, setResult] = React.useState<string | undefined>();
 
+  var loggerObj = new LoggerObject('MyApp');
+
   React.useEffect(() => {
-    loggerLibrary().then((res) => setResult(res.type));
+    // loggerLibrary().then((res) => setResult(res.type));
+    loggerObj.info('This is a debug message');
 
   }, [result]);
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,42 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+// import FileLogger from 'react-native-file-logger';
+
+// FileLogger.configure({
+//   logLevel: FileLogger.levels.INFO,
+//   maxFiles: 5,
+//   maxSize: 5000000,
+// });
+
+class LoggerObject {
+  
+  name: string;
+
+  constructor(name?: string) {
+    this.name = name || 'Logger';
+  }
+
+  format(level: string, message: string): string {
+    return `[${this.name}] [${level}] ${message}`;
+  }
+
+  debug(message: string): void {
+    //     FileLogger.debug(this.format('debug', message));
+    console.log(this.format('debug', message));
+  }
+
+  info(message: string): void {
+    //     FileLogger.info(this.format('info', message));
+    console.info(this.format('info', message));
+  }
+
+  warn(message: string): void {
+    //     FileLogger.warn(this.format('warn', message));
+    console.warn(this.format('warn', message));
+  }
+
+  error(message: string): void {
+    //     FileLogger.error(this.format('error', message));
+    console.error(this.format('error', message));
+  }
 }
+
+export default LoggerObject;
